4fd3b159617869840aed017acf85b36833a2f3d4
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-10-19 15:39:15 +0530

    chore: configured directories

---
+++ src/features/home/screens/home.screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { HomeScreenView } from "../components/styles/home.styles";
+
+export const HomeScreen = () => {
+  return (
+    <HomeScreenView>
+      <Text>Home Screen</Text>
+    </HomeScreenView>
+  );
+};
