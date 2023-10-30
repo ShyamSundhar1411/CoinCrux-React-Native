cd6d259723d0cb9b79a649f0f0c13ab44d273c51
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-10-30 16:10:08 +0530

    feat: added PanResponder for FeedScreen

--- firebase.config.js
+++ firebase.config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC4M4EtrppMgPbyGIhvN1ULc5SUeZSgy14",
+  authDomain: "coincrux-12be2.firebaseapp.com",
+  projectId: "coincrux-12be2",
+  storageBucket: "coincrux-12be2.appspot.com",
+  messagingSenderId: "87632180660",
+  appId: "1:87632180660:web:095dcbb457e80d7e4daee1",
+  measurementId: "G-9XB54QC4J1",
 };
 
 const app = initializeApp(firebaseConfig);
--- src/features/feed/components/FeedCarouselComponent.js
+++ src/features/feed/components/FeedCarouselComponent.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Dimensions, Image, Text, View } from "react-native";
+import React, { Component } from "react";
+import { Dimensions, Image, View, Animated, PanResponder } from "react-native";
 import {
   FeedHeading,
   MyFeedScreenView,
@@ -8,27 +8,49 @@ import {
 import { AssetChip } from "../../../components/styles/global.styles";
 import { TimeAgo } from "../../../utils/time.function";
 
-export const FeedComponent = ({ data }) => {
-  const width = Dimensions.get("window").width;
-  const height = Dimensions.get("window").height;
-  return (
-    <>
-      {data.map((item, index) => {
-        const time = TimeAgo({ time: item.createdAt });
-        return (
-          <MyFeedScreenView key={index} height={height} width={width}>
-            <Image
-              source={{ uri: item.coinImage }}
-              style={{ resizeMode: "cover", height: 200, width: width }}
-            />
-            <View style={{ padding: 10 }}>
-              <AssetChip>{item.assetName}</AssetChip>
-              <FeedHeading>{item.coinHeading}</FeedHeading>
-              <FeedDescription>{time}</FeedDescription>
-            </View>
-          </MyFeedScreenView>
-        );
-      })}
-    </>
-  );
-};
+class FeedComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+    };
+    this.position = new Animated.ValueXY();
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        this.position.setValue({ y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {},
+    });
+  }
+  render() {
+    const { data } = this.props;
+    const width = Dimensions.get("window").width;
+    const height = Dimensions.get("window").height;
+
+    return (
+      <>
+        {data.map((item, index) => {
+          const time = TimeAgo({ time: item.createdAt });
+          return (
+            <Animated.View key={index}>
+              <MyFeedScreenView height={height} width={width}>
+                <Image
+                  source={{ uri: item.coinImage }}
+                  style={{ resizeMode: "cover", height: 200, width: width }}
+                />
+                <View style={{ padding: 10 }}>
+                  <AssetChip>{item.assetName}</AssetChip>
+                  <FeedHeading>{item.coinHeading}</FeedHeading>
+                  <FeedDescription>{time}</FeedDescription>
+                </View>
+              </MyFeedScreenView>
+            </Animated.View>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default FeedComponent;
